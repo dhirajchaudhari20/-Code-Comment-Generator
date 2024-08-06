230f7ae1b7448e5e33cb90388cf3fcf8f551b6ce
Dhiraj Chaudhari <117934434+dhirajchaudhari20@users.noreply.github.com>
2024-08-06 22:16:20 +0530

    Update app.py

--- app.py
+++ app.py
@@ -13,7 +13,7 @@ def load_models():
     """
 
     # Setup the Google Gemini AI API Key
-    google_api_key = st.secrets["GOOGLE_API_KEY"]
+    google_api_key = st.secrets["AIzaSyAPrrQLkezoZZjhfwYlpP9SHpdVvJPOXQI"]
     genai.configure(api_key=google_api_key)
     text_model_pro = genai.GenerativeModel('gemini-pro')
     return text_model_pro
@@ -161,7 +161,7 @@ def generative_config():
     return config
 
 def custom_footer():
-    footer = ''' Made with <svg viewBox="0 0 1792 1792" preserveAspectRatio="xMidYMid meet" xmlns="http://www.w3.org/2000/svg" style="height: 0.8rem;"><path d="M896 1664q-26 0-44-18l-624-602q-10-8-27.5-26T145 952.5 77 855 23.5 734 0 596q0-220 127-344t351-124q62 0 126.5 21.5t120 58T820 276t76 68q36-36 76-68t95.5-68.5 120-58T1314 128q224 0 351 124t127 344q0 221-229 450l-623 600q-18 18-44 18z" fill="#e25555"></path></svg> by Shubham Sah'''
+    footer = ''' Made with <svg viewBox="0 0 1792 1792" preserveAspectRatio="xMidYMid meet" xmlns="http://www.w3.org/2000/svg" style="height: 0.8rem;"><path d="M896 1664q-26 0-44-18l-624-602q-10-8-27.5-26T145 952.5 77 855 23.5 734 0 596q0-220 127-344t351-124q62 0 126.5 21.5t120 58T820 276t76 68q36-36 76-68t95.5-68.5 120-58T1314 128q224 0 351 124t127 344q0 221-229 450l-623 600q-18 18-44 18z" fill="#e25555"></path></svg> by Dhiraj and Team'''
     st.markdown(footer, unsafe_allow_html=True)
 
 # main Function 
